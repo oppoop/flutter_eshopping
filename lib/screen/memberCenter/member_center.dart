dc27b8f27dad056e2afbc9618570731a31b685c7
oppoop <d0350315@gmail.com>
2021-08-17 02:03:58 +0800

    會員中心頭像UI

--- lib/screen/memberCenter/member_center.dart
+++ lib/screen/memberCenter/member_center.dart
@@ -2,6 +2,7 @@ import 'package:flutter_eshopping/generated/l10n.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_eshopping/utils/app_libs.dart';
 class MemberCenter extends StatefulWidget {
   @override
   _MemberCenter createState() => _MemberCenter();
@@ -18,6 +19,15 @@ class _MemberCenter extends State<MemberCenter> {
           title: Text(
             S().membercenter,
           ),
+          flexibleSpace: Container(
+              decoration:BoxDecoration(
+                  gradient: LinearGradient(
+                      colors:[Colors.indigo,Colors.purple],
+                      begin: Alignment.bottomRight,
+                      end: Alignment.topLeft
+                  )
+              )
+          ),
         ),
         body: memberCenter()
     );
@@ -26,14 +36,48 @@ class _MemberCenter extends State<MemberCenter> {
     return Container(
       child: Column(
         children: [
-          Container(
-            color: Colors.grey,
-            height: MediaQuery.of(context).size.height*0.15,
-            child: Center(
-              child: IconButton(icon: Icon(Icons.camera_enhance,size: 30,),onPressed: (){},),
-            ),
-          ),
-          ListTile(
+          Stack(
+            alignment: Alignment.bottomLeft,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      border: Border(bottom: BorderSide(color: Colors.black,width: 3))
+                    ),
+                    height: MediaQuery.of(context).size.height*0.17,
+                    child: Center(
+                      child: IconButton(icon: Icon(Icons.camera_enhance,size: 30,),onPressed: (){},),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: MediaQuery.of(context).size.height*0.09,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(width: 10,),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all(color: Colors.black,width: 4)
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: AppLibScreen.imageContent(
+                        imageOnePath:
+                        'https://www.steamxo.com/wp-content/uploads/2019/11/5ggL5q154529_874683.jpg',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
