79ca66bb309a59ee886402fc91fac256c7995fff
oppoop <d0350315@gmail.com>
2021-08-24 00:55:06 +0800

    新增瀏覽紀錄

--- lib/screen/browsingRecords_screen.dart
+++ lib/screen/browsingRecords_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_eshopping/generated/l10n.dart';
 import 'package:google_fonts/google_fonts.dart';
-class RecommendBar extends StatelessWidget {
+class BrowsingRecordsBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text(S().newProduct,style: GoogleFonts.notoSerif()
+          Text(S().browsingRecords,style: GoogleFonts.notoSerif()
               .copyWith()),
         ],
       ),
@@ -16,12 +16,8 @@ class RecommendBar extends StatelessWidget {
   }
 }
 
-class Recommend extends StatefulWidget {
-  @override
-  _RecommendState createState() => _RecommendState();
-}
 
-class _RecommendState extends State<Recommend> {
+class BrowsingRecordState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
